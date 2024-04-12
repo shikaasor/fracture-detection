0e278d23db9e037fe42c7f528956f18784da540a
kaasor <asor.tar23@gmail.com>
2024-04-12 19:30:06 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ObjectDetection',
+    version = '0.0.0',
+    author = 'Kaasor',
+    author_email = 'asor.tar23@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
